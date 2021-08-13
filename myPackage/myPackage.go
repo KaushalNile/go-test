ed39bb0eaf3421d90f517d99b7382ad1f4d6680b
MKCLINDIA\kaushaln <kaushaln@mkcl.org>
2021-08-13 12:27:54 +0530

    Initial Commit

---
+++ myPackage/myPackage.go
@@ -0,0 +1,8 @@
+package myPackage
+
+func Calc() int {
+	a := 20
+	b := 10
+
+	return a + b
+}
---
+++ test.go
@@ -0,0 +1,10 @@
+package main
+
+import (
+	"demoPackage/myPackage"
+	"fmt"
+)
+
+func main() {
+	fmt.Println(myPackage.Calc())
+}
